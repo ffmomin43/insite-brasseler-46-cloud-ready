74c366f56f8a3ed667ae12b1823d30eaed803c09
Fahad Momin <ffmomin43@gmail.com>
2020-08-24 17:59:51 +0530

    commented some files
    PunchOutCustomerOrderProcessorBrasseler
    PunchOutOrderRequestServiceBrasseler
    PunchOutOrderRequestUtilitiesBrasseler
    PunchOutProviderMessageFactoryBrasseler
    PunchOutSetupRequestService_Brasseler
    PunchOutSubmitPoRequisitionServiceBrasseler
    IntegrationJobSchedulingService_Brasseler

--- Extention/InSiteCommerce.Brasseler.Integration/WebService/IntegrationJobSchedulingService_Brasseler.cs
+++ Extention/InSiteCommerce.Brasseler.Integration/WebService/IntegrationJobSchedulingService_Brasseler.cs
@@ -7,31 +7,31 @@ using Insite.Integration.WebService.SystemSettings;
 
 namespace InSiteCommerce.Brasseler.Integration.WebService
 {
-    public class IntegrationJobSchedulingService_Brasseler : IntegrationJobSchedulingService
-    {
+    //public class IntegrationJobSchedulingService_Brasseler : IntegrationJobSchedulingService
+    //{
 
-        public IntegrationJobSchedulingService_Brasseler(IUnitOfWorkFactory unitOfWorkFactory, IntegrationGeneralSettings IntegrationGeneralSettings) : base(unitOfWorkFactory, IntegrationGeneralSettings)
-        {
-        }
+    //    public IntegrationJobSchedulingService_Brasseler(IUnitOfWorkFactory unitOfWorkFactory, IntegrationGeneralSettings IntegrationGeneralSettings) : base(unitOfWorkFactory, IntegrationGeneralSettings)
+    //    {
+    //    }
 
-        protected override void SetParameters(IUnitOfWork unitOfWork, JobDefinition jobDefinition, Collection<JobDefinitionStepParameter> parameters, string genericParameter)
-        {
-            base.SetParameters(unitOfWork, jobDefinition, parameters, genericParameter);
-            foreach (JobDefinitionStep jobDefinitionStep in jobDefinition.JobDefinitionSteps)
-            {
-                foreach (JobDefinitionStepParameter definitionStepParameter1 in jobDefinitionStep.JobDefinitionStepParameters)
-                {
-                    JobDefinitionStepParameter stepParam = definitionStepParameter1;
-                    if (jobDefinition.Name.EqualsIgnoreCase("SmartSupply Submit Job"))
-                    {
-                        if (stepParam.Name.EqualsIgnoreCase("SubscriptionOrderId"))
-                        {
-                            stepParam.Value = genericParameter;
-                            parameters.Add(stepParam);
-                        }
-                    }
-                }
-            }
-        }
-    }
+    //    protected override void SetParameters(IUnitOfWork unitOfWork, JobDefinition jobDefinition, Collection<JobDefinitionStepParameter> parameters, string genericParameter)
+    //    {
+    //        base.SetParameters(unitOfWork, jobDefinition, parameters, genericParameter);
+    //        foreach (JobDefinitionStep jobDefinitionStep in jobDefinition.JobDefinitionSteps)
+    //        {
+    //            foreach (JobDefinitionStepParameter definitionStepParameter1 in jobDefinitionStep.JobDefinitionStepParameters)
+    //            {
+    //                JobDefinitionStepParameter stepParam = definitionStepParameter1;
+    //                if (jobDefinition.Name.EqualsIgnoreCase("SmartSupply Submit Job"))
+    //                {
+    //                    if (stepParam.Name.EqualsIgnoreCase("SubscriptionOrderId"))
+    //                    {
+    //                        stepParam.Value = genericParameter;
+    //                        parameters.Add(stepParam);
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }
--- Extention/InSiteCommerce.Brasseler/Punchout/PunchOutOrderRequestServiceBrasseler.cs
+++ Extention/InSiteCommerce.Brasseler/Punchout/PunchOutOrderRequestServiceBrasseler.cs
@@ -17,80 +17,80 @@ using System.Web;
 
 namespace InSiteCommerce.Brasseler.Punchout
 {
-    public class PunchOutOrderRequestServiceBrasseler: PunchOutOrderRequestService
-    {
+    //public class PunchOutOrderRequestServiceBrasseler: PunchOutOrderRequestService
+    //{
 
-        public PunchOutOrderRequestServiceBrasseler(IUnitOfWorkFactory unitOfWorkFactory, IPunchOutOrderRequestParser punchOutOrderRequestParser, IPunchOutProviderMessageFactory punchOutProviderMessageFactory, IPunchOutRequestValidator requestValidator, IArgumentContract argumentContract, IPunchOutOrderRequestMessageFormatValidator orderRequestMessageValidator, IPunchOutCustomerOrderProcessor customerOrderSubmitter, IPunchOutRequestObfuscator punchOutRequestObfuscator, IPunchOutOrderRequestUtilities punchOutOrderRequestUtilities, IPunchOutOrderRequestMessageLogger punchOutOrderRequestMessageLogger, PunchoutSettings punchoutSettings, IEmailService emailService) : base(unitOfWorkFactory,punchOutOrderRequestParser, punchOutProviderMessageFactory, requestValidator,argumentContract, orderRequestMessageValidator, customerOrderSubmitter, punchOutRequestObfuscator, punchOutOrderRequestUtilities, punchOutOrderRequestMessageLogger, punchoutSettings, emailService) {}
+    //    public PunchOutOrderRequestServiceBrasseler(IUnitOfWorkFactory unitOfWorkFactory, IPunchOutOrderRequestParser punchOutOrderRequestParser, IPunchOutProviderMessageFactory punchOutProviderMessageFactory, IPunchOutRequestValidator requestValidator, IArgumentContract argumentContract, IPunchOutOrderRequestMessageFormatValidator orderRequestMessageValidator, IPunchOutCustomerOrderProcessor customerOrderSubmitter, IPunchOutRequestObfuscator punchOutRequestObfuscator, IPunchOutOrderRequestUtilities punchOutOrderRequestUtilities, IPunchOutOrderRequestMessageLogger punchOutOrderRequestMessageLogger, PunchoutSettings punchoutSettings, IEmailService emailService) : base(unitOfWorkFactory,punchOutOrderRequestParser, punchOutProviderMessageFactory, requestValidator,argumentContract, orderRequestMessageValidator, customerOrderSubmitter, punchOutRequestObfuscator, punchOutOrderRequestUtilities, punchOutOrderRequestMessageLogger, punchoutSettings, emailService) {}
 
-        public override string HandlePunchOutOrderMessage(string xmlRequest, string ipAddress, string currentDomain)
-        {
-            IRepository<PunchOutOrderRequest> repository = this.UnitOfWork.GetRepository<PunchOutOrderRequest>();
-            PunchOutOrderRequest punchOutOrderRequest1 = new PunchOutOrderRequest()
-            {
-                RequestUrl = HttpContext.Current.Request.Url.AbsoluteUri
-            };
-            repository.Insert(punchOutOrderRequest1);
-            this.UnitOfWork.Save();
-            try
-            {
-                this.UnitOfWork.BeginTransaction();
-                if (!this.OrderRequestMessageValidator.IsValidRequest(xmlRequest))
-                    throw new ArgumentException("The PunchOutOrder Request is not in a known format");
-                punchOutOrderRequest1 = this.PunchOutOrderRequestParser.CreatePunchOutOrderMessageFromXml(punchOutOrderRequest1, xmlRequest, currentDomain);
-                this.UnitOfWork.Save();
-                this.UnitOfWork.CommitTransaction();
-                this.UnitOfWork.Clear(true);
-                string secretWord = punchOutOrderRequest1.SecretWord;
-                punchOutOrderRequest1 = this.UnitOfWork.GetRepository<PunchOutOrderRequest>().Get(punchOutOrderRequest1.Id);
-                punchOutOrderRequest1.SecretWord = secretWord;
-                this.UnitOfWork.BeginTransaction();
-                PunchOutValidationResult validationResult = this.RequestValidator.ValidateRequest((IPunchOutValidatable)punchOutOrderRequest1);
-                Tuple<string, string> tuple = validationResult.IsValid ? new Tuple<string, string>(this.ResultCode(PunchOutMessageResultCode.Success), "OK") : new Tuple<string, string>(this.ResultCode(PunchOutMessageResultCode.Error), validationResult.Message);
-                if (!validationResult.IsValid)
-                    throw new ArgumentException(validationResult.Message);
-                punchOutOrderRequest1.UserProfile = validationResult.UserProfile;
-                punchOutOrderRequest1.UserProfileId = new Guid?(validationResult.UserProfile.Id);
-                this.UnitOfWork.Save();
-                string auxilliartPartId = this.PunchOutOrderRequestUtilities.GetSupplierAuxilliartPartId(punchOutOrderRequest1);
+    //    public override string HandlePunchOutOrderMessage(string xmlRequest, string ipAddress, string currentDomain)
+    //    {
+    //        IRepository<PunchOutOrderRequest> repository = this.UnitOfWork.GetRepository<PunchOutOrderRequest>();
+    //        PunchOutOrderRequest punchOutOrderRequest1 = new PunchOutOrderRequest()
+    //        {
+    //            RequestUrl = HttpContext.Current.Request.Url.AbsoluteUri
+    //        };
+    //        repository.Insert(punchOutOrderRequest1);
+    //        this.UnitOfWork.Save();
+    //        try
+    //        {
+    //            this.UnitOfWork.BeginTransaction();
+    //            if (!this.OrderRequestMessageValidator.IsValidRequest(xmlRequest))
+    //                throw new ArgumentException("The PunchOutOrder Request is not in a known format");
+    //            punchOutOrderRequest1 = this.PunchOutOrderRequestParser.CreatePunchOutOrderMessageFromXml(punchOutOrderRequest1, xmlRequest, currentDomain);
+    //            this.UnitOfWork.Save();
+    //            this.UnitOfWork.CommitTransaction();
+    //            this.UnitOfWork.Clear(true);
+    //            string secretWord = punchOutOrderRequest1.SecretWord;
+    //            punchOutOrderRequest1 = this.UnitOfWork.GetRepository<PunchOutOrderRequest>().Get(punchOutOrderRequest1.Id);
+    //            punchOutOrderRequest1.SecretWord = secretWord;
+    //            this.UnitOfWork.BeginTransaction();
+    //            PunchOutValidationResult validationResult = this.RequestValidator.ValidateRequest((IPunchOutValidatable)punchOutOrderRequest1);
+    //            Tuple<string, string> tuple = validationResult.IsValid ? new Tuple<string, string>(this.ResultCode(PunchOutMessageResultCode.Success), "OK") : new Tuple<string, string>(this.ResultCode(PunchOutMessageResultCode.Error), validationResult.Message);
+    //            if (!validationResult.IsValid)
+    //                throw new ArgumentException(validationResult.Message);
+    //            punchOutOrderRequest1.UserProfile = validationResult.UserProfile;
+    //            punchOutOrderRequest1.UserProfileId = new Guid?(validationResult.UserProfile.Id);
+    //            this.UnitOfWork.Save();
+    //            string auxilliartPartId = this.PunchOutOrderRequestUtilities.GetSupplierAuxilliartPartId(punchOutOrderRequest1);
                 
-                PunchOutSession punchOutSession;
-                if (string.IsNullOrEmpty(auxilliartPartId))
-                    punchOutSession = (PunchOutSession)null;
-                else
-                {
-                    Guid puchOutSessionId = new Guid(auxilliartPartId);
-                    punchOutSession = this.UnitOfWork.GetRepository<PunchOutSession>().GetTable().Expand<PunchOutSession, CustomerOrder>((Expression<Func<PunchOutSession, CustomerOrder>>)(p => p.CustomerOrder)).First<PunchOutSession>((Expression<Func<PunchOutSession, bool>>)(p => p.Id == puchOutSessionId));
-                }
-                this.CustomerOrderSubmitter.ProcessPunchOutOrder(punchOutSession, punchOutOrderRequest1);
-                string orderRequestResponse = this.PunchOutProviderMessageFactory.CreateOrderRequestResponse(tuple.Item1, tuple.Item2, currentDomain);
-                punchOutOrderRequest1.OrderResponse = orderRequestResponse;
-                this.UnitOfWork.Save();
-                this.UnitOfWork.CommitTransaction();
-                this.UnitOfWork.Clear(true);
-                punchOutOrderRequest1 = this.UnitOfWork.GetRepository<PunchOutOrderRequest>().Get(punchOutOrderRequest1.Id);
-                punchOutOrderRequest1.Status = this.RetrievePunchOutOrderRequestStatus(punchOutOrderRequest1).ToString();
-                this.PunchOutOrderRequestMessageLogger.AddPunchOutOrderRequestMessage(punchOutOrderRequest1, PunchOutOrderRequestMessageType.Info, string.Format("Successfully processed order request. Web Order Number: {0}", (object)punchOutOrderRequest1.CustomerOrder.OrderNumber));
-                this.UnitOfWork.Save();
-                return orderRequestResponse;
-            }
-            catch (Exception ex)
-            {
-                this.PunchOutOrderRequestMessageLogger.AddPunchOutOrderRequestMessage(punchOutOrderRequest1, PunchOutOrderRequestMessageType.Fatal, "  OrderID :   " + punchOutOrderRequest1.OrderId + " failed for " + ex.StackTrace.ToString() + ex.Message);
-                this.UnitOfWork.RollbackTransaction();
-                this.UnitOfWork.Clear(true);
-                this.PunchOutOrderRequestMessageLogger.AddPunchOutOrderRequestMessage(punchOutOrderRequest1, PunchOutOrderRequestMessageType.Fatal, ex.Message);
-                string orderRequestResponse = this.PunchOutProviderMessageFactory.CreateOrderRequestResponse(this.ResultCode(PunchOutMessageResultCode.Error), ex.Message, currentDomain);
-                PunchOutOrderRequest punchOutOrderRequest2 = repository.Get(punchOutOrderRequest1.Id);
-                punchOutOrderRequest2.PunchOutOrderRequestCXml = this.PunchOutRequestObfuscator.Obfuscate(xmlRequest);
-                punchOutOrderRequest2.OrderResponse = orderRequestResponse;
-                this.UnitOfWork.Save();
-                this.UnitOfWork.Clear(true);
-                PunchOutOrderRequest punchOutOrderRequest3 = this.UnitOfWork.GetRepository<PunchOutOrderRequest>().Get(punchOutOrderRequest2.Id);
-                punchOutOrderRequest3.Status = this.RetrievePunchOutOrderRequestStatus(punchOutOrderRequest3).ToString();
-                this.SendPunchOutFailedOrderEmail(punchOutOrderRequest3);
-                this.UnitOfWork.Save();
-                return orderRequestResponse;
-            }
-        }
-    }
+    //            PunchOutSession punchOutSession;
+    //            if (string.IsNullOrEmpty(auxilliartPartId))
+    //                punchOutSession = (PunchOutSession)null;
+    //            else
+    //            {
+    //                Guid puchOutSessionId = new Guid(auxilliartPartId);
+    //                punchOutSession = this.UnitOfWork.GetRepository<PunchOutSession>().GetTable().Expand<PunchOutSession, CustomerOrder>((Expression<Func<PunchOutSession, CustomerOrder>>)(p => p.CustomerOrder)).First<PunchOutSession>((Expression<Func<PunchOutSession, bool>>)(p => p.Id == puchOutSessionId));
+    //            }
+    //            this.CustomerOrderSubmitter.ProcessPunchOutOrder(punchOutSession, punchOutOrderRequest1);
+    //            string orderRequestResponse = this.PunchOutProviderMessageFactory.CreateOrderRequestResponse(tuple.Item1, tuple.Item2, currentDomain);
+    //            punchOutOrderRequest1.OrderResponse = orderRequestResponse;
+    //            this.UnitOfWork.Save();
+    //            this.UnitOfWork.CommitTransaction();
+    //            this.UnitOfWork.Clear(true);
+    //            punchOutOrderRequest1 = this.UnitOfWork.GetRepository<PunchOutOrderRequest>().Get(punchOutOrderRequest1.Id);
+    //            punchOutOrderRequest1.Status = this.RetrievePunchOutOrderRequestStatus(punchOutOrderRequest1).ToString();
+    //            this.PunchOutOrderRequestMessageLogger.AddPunchOutOrderRequestMessage(punchOutOrderRequest1, PunchOutOrderRequestMessageType.Info, string.Format("Successfully processed order request. Web Order Number: {0}", (object)punchOutOrderRequest1.CustomerOrder.OrderNumber));
+    //            this.UnitOfWork.Save();
+    //            return orderRequestResponse;
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            this.PunchOutOrderRequestMessageLogger.AddPunchOutOrderRequestMessage(punchOutOrderRequest1, PunchOutOrderRequestMessageType.Fatal, "  OrderID :   " + punchOutOrderRequest1.OrderId + " failed for " + ex.StackTrace.ToString() + ex.Message);
+    //            this.UnitOfWork.RollbackTransaction();
+    //            this.UnitOfWork.Clear(true);
+    //            this.PunchOutOrderRequestMessageLogger.AddPunchOutOrderRequestMessage(punchOutOrderRequest1, PunchOutOrderRequestMessageType.Fatal, ex.Message);
+    //            string orderRequestResponse = this.PunchOutProviderMessageFactory.CreateOrderRequestResponse(this.ResultCode(PunchOutMessageResultCode.Error), ex.Message, currentDomain);
+    //            PunchOutOrderRequest punchOutOrderRequest2 = repository.Get(punchOutOrderRequest1.Id);
+    //            punchOutOrderRequest2.PunchOutOrderRequestCXml = this.PunchOutRequestObfuscator.Obfuscate(xmlRequest);
+    //            punchOutOrderRequest2.OrderResponse = orderRequestResponse;
+    //            this.UnitOfWork.Save();
+    //            this.UnitOfWork.Clear(true);
+    //            PunchOutOrderRequest punchOutOrderRequest3 = this.UnitOfWork.GetRepository<PunchOutOrderRequest>().Get(punchOutOrderRequest2.Id);
+    //            punchOutOrderRequest3.Status = this.RetrievePunchOutOrderRequestStatus(punchOutOrderRequest3).ToString();
+    //            this.SendPunchOutFailedOrderEmail(punchOutOrderRequest3);
+    //            this.UnitOfWork.Save();
+    //            return orderRequestResponse;
+    //        }
+    //    }
+    //}
 }
